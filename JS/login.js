b9bc86d2d4d1ee483b1aae2468130fe8d4de787d
Bultut-yegon <bultut9785@gmail.com>
2024-02-07 09:18:53 +0300

    fun

---
+++ JS/login.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBbMbTOH3NHv6fp4jAT3HA3vA6-Ur23In4",
+    authDomain: "group-12-kcb-lms.firebaseapp.com",
+    projectId: "group-12-kcb-lms",
+    storageBucket: "group-12-kcb-lms.appspot.com",
+    messagingSenderId: "15476877993",
+    appId: "1:15476877993:web:1c45be65570b120d48d7b8"
+  };
+  const app= initializeApp(firebaseConfig)
